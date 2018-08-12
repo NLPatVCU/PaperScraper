a71faac4d55c3e19d1b31d3cb45a9f81686fd122
Andriy Mulyar <andriy.mulyar@gmail.com>
2018-08-12 00:07:14 -0400

    Initial data commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='paperscraper',
+    version='0.0.1',
+    packages=['paperscraper'],
+    url='',
+    license='',
+    author='Andriy Mulyar',
+    author_email='contact@andriymulyar.com',
+    description=''
+)
